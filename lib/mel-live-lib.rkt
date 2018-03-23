7a9d0da534452845966d7b4573e960a168f73cdf
dajin <dajinchu@gmail.com>
2018-03-23 09:53:23 -0400

    mel-live-lib provides

--- lib/mel-live-lib.rkt
+++ lib/mel-live-lib.rkt
@@ -2,6 +2,15 @@
 
 (require rsound)
 
+(module+ test
+         (require rackunit))
+
+(provide
+  update-tempo
+  play-song
+  (rename-out 
+    [s-bassdrum bassdrum]))
+
 ;; Data Definitions
 
 ; A Song is a [Listof Loop]
@@ -24,6 +33,13 @@
 (define stream (make-pstream))
 (define beat-length (* (/ (default-sample-rate) tempo) 60))
 
+; N -> (void)
+; Update the tempo and beat length
+; EFFECT sets the tempo and beat length.
+(define (update-tempo t)
+  (set! tempo t)
+  (set! beat-length (* (/ (default-sample-rate) current-tempo) 60)))
+
 ; Sound N -> (void)
 ; Queues a sound onto the stream to play at a certain beat number.
 (define (play-at-beat sound beat-num)
@@ -33,11 +49,17 @@
 
 ; N -> N
 ; Calculate the beat that the specified frame belongs to.
+(module+ test
+         (check-equal? (frame->beat 100)
+                       (floor (/ 100 beat-length))))
 (define (frame->beat frame)
   (floor (/ frame beat-length)))
 
 ; N -> N
 ; Calculate the first frame of the specified beat.
+(module+ test
+         (check-equal? (beat->frame 2)
+                       (round (* 2 beat-length))))
 (define (beat->frame beat)
   (round (* beat beat-length)))
 
@@ -51,15 +73,20 @@
 (define (next-beat-frame)
   (beat->frame (add1 (current-beat))))
 
+(define s-bassdrum (rs-scale .5 bassdrum))
 
-(define ts (rs-scale .5 bassdrum))
+; QUEUE-SIZE defines how many beats to queue at once.
+(define QUEUE-SIZE 3)
+
+; QUEUE-AHEAD defines how far in advance to queue sounds, in beats.
+(define QUEUE-AHEAD 3)
 
 ; Song -> (void)
 ; play-song runs indefinitely, playing the specified song.
+; EFFECT plays sounds FOREVER
 (define (play-song song)
-    (play-at-beat ts (+ 3 (current-beat)))
-    (play-at-beat ts (+ 4 (current-beat)))
-    (play-at-beat ts (+ 5 (current-beat)))
+    (for ([i QUEUE-SIZE])
+        (play-at-beat ts (+ QUEUE-AHEAD i (current-beat))))
     (sleep (/ (- (beat->frame (+ 3 (current-beat))) (pstream-current-frame stream)) (default-sample-rate)))
     (play-song song))
-(play-song 's)
+
